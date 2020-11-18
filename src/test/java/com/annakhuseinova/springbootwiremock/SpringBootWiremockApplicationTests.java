cab3dcbf24ecc224540a846372b588cf6c608e04
annakhuseinova <annakhuseinova@yandex.ru>
2020-11-18 22:24:29 +0300

    Initial

---
+++ src/test/java/com/annakhuseinova/springbootwiremock/SpringBootWiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springbootwiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWiremockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
